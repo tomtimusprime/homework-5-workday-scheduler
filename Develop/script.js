98db0fe20fe47d25eda7b9b41107c4822261c7a9
Tom Black <tom.black06007@gmail.com>
2020-04-02 16:18:58 -0700

    Updated the text area and added the save button on each row.

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    console.log(moment());
+    let now = moment();
+
+    $("#submit").on("click", function() {
+        alert("The submit button was clicked");
+    })
+    
+});
