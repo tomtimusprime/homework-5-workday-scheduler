8de8fbf478f0100e58e5ace2ad1cca7dad6dd36c
Tom Black <tom.black06007@gmail.com>
2020-04-02 18:40:29 -0700

    Handles the different textareas with their associated save buttons to make sure that data is being handled accordingly.

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
-    console.log(moment());
-    let now = moment();
+    let nowEl = $("#currentDay");
+    nowEl.text(moment());
+    
+    for(let i = 8; i < 17; i++) {
+       
+        $("a[data-time=" + i + "am]").on("click", function(event) {
+            event.preventDefault();
+            let dataTime = $("textarea[data-time=" + i + "am]");
+            console.log(dataTime.val());
+            
+            
+            // let clicked = $(this).val();
+            // alert(clicked);
+            
+    
+        })
 
-    $("#submit").on("click", function() {
-        alert("The submit button was clicked");
-    })
+    }
+    
+
+
+    
     
 });
+
+//RX JS - libraries that handles observables
