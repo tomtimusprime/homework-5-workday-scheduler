9c06111b72c0c56026be2ad24974304c54608df8
Tom Black <tom.black06007@gmail.com>
2020-04-04 16:49:28 -0700

    Made some additional changes to the styling, got the local storage all worked out and updated the readme.

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-## User Story
-
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+This is a simple calendar application. If you click on a time block for a standard work day (i.e. 8am to 5pm) and write a calendar item and then click the save button, that information will be saved to localStorage and persist in the browser, even if you refresh the page. If the time block gray, it means the time has already passed. If the timeblock is red it means that's the current hour and if green it means those are future time slots. It also displays the current date and time at the top of the page.
 
 ## Acceptance Criteria
 
@@ -30,17 +20,5 @@ WHEN I refresh the page
 THEN the saved events persist
 ```
 
-The following animation demonstrates the application functionality:
 
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
 
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -1,39 +1,36 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
 
 $(document).ready(function () {
-    let nowEl = $("#currentDay");
-    const currentDay = moment().format("dddd MM-DD-YY");
-    const displayNowEl = moment().format("dddd, MMMM Do YYYY, h:mm a");
-    nowEl.text(displayNowEl);
-    //Get data from local storage 
-    let scheduleData = getScheduleData();
-    saveData(scheduleData, currentDay);
-    const $timeBlocks = $(".text-area");
-    console.log(scheduleData);
-    console.log($timeBlocks);
-    updateTimeBlocks($timeBlocks, scheduleData);
-    //and put it in the right spots on the page
-    //Color code red for current hour and then green for future, for past gray or blue or whatever
+    setUp();
+
+    function addListeners() {
+        let rowDivEl = $(".row");
+        rowDivEl.each((index, element) => {
+            let btn = $(element).find(".fa-save");
+            btn.on("click", event => {
+                let button = $(event.target);
+                let buttonIndex = button.data("time");
+                let scheduleData = getScheduleData();
+                let eventText = $(element).find(".text-area").val();
+                console.log(buttonIndex, scheduleData, eventText, button);
+                scheduleData[buttonIndex].text = eventText;
+                let currentDay = getCurrentDay();
+                saveData(scheduleData, currentDay);
+            })
+        })
+    }
+
+    function getCurrentDay() {
+        return moment().format("dddd MM-DD-YY");
+        //return moment().add(1, 'days').format("dddd MM-DD-YY");
+    }
 
 
-
-    function getScheduleData(currentDay) {
-        
-        let data = JSON.parse(localStorage.getItem(currentDay));
-        if(!data) {
+    function getScheduleData() {
+        let currentDay = getCurrentDay();
+        let data = localStorage.getItem(currentDay);
+        data = JSON.parse(data);
+        console.log(data);
+        if (!data) {
             data = generateScheduleData();
         }
         return data;
@@ -86,14 +83,14 @@ $(document).ready(function () {
 
     function updateTimeBlocks($timeBlocks, scheduleData) {
         let currentHour = moment().hour();
-        $timeBlocks.each((index, element)=> {
+        $timeBlocks.each((index, element) => {
             let blockData = scheduleData[index];
             let currentBlock = $(element);
-            if(currentHour > blockData.hour) {
+            if (currentHour > blockData.hour) {
                 //add css class for past block
                 currentBlock.addClass("past");
             }
-            else if(currentHour === blockData.hour) {
+            else if (currentHour === blockData.hour) {
                 //add css class for present block
                 currentBlock.addClass("present");
             }
@@ -103,41 +100,30 @@ $(document).ready(function () {
             }
             //put the item on the dom
             currentBlock.text(blockData.text);
-            
+
         });
-        
-       
     }
 
     function saveData(scheduleData, currentDay) {
         localStorage.setItem(currentDay, JSON.stringify(scheduleData));
-    
+
     }
 
-    //when user clicks save you want to get the text from the data-text area
-    //get your current schedule data.
-    //update the appropriate item in the array with the event text. 
-    //Save schedule data to localstorage.
+    function setUp() {
+        let nowEl = $("#currentDay");
+        const currentDay = getCurrentDay();
+        const displayNowEl = moment().format("dddd, MMMM Do YYYY, h:mm a");
+        nowEl.text(displayNowEl);
+        //Get data from local storage 
+        let scheduleData = getScheduleData();
+        saveData(scheduleData, currentDay);
+        const $timeBlocks = $(".text-area");
+        console.log(scheduleData);//*
+        console.log($timeBlocks);//*
+        updateTimeBlocks($timeBlocks, scheduleData);
+        addListeners();
+    }
 });
 
 //RX JS - libraries that handles observables
 
- // for (let i = 8; i <= 17; i++) {
-        //     let dataTime;
-        //     if (i < 12) {
-        //         $("a[data-time=" + i + "am]").on("click", function (event) {
-        //             event.preventDefault();
-        //             dataTime = $("textarea[data-time=" + i + "am]");
-        //             //itemsArray[i] = dataTime.val();
-        //             console.log(dataTime.val());
-        //         })
-        //     }
-    
-        //     if (i >= 12)
-        //         $("a[data-time=" + i + "pm]").on("click", function (event) {
-        //             event.preventDefault();
-        //             dataTime = $("textarea[data-time=" + i + "pm]");
-        //             console.log(dataTime.val());
-        //         })
-    
-        // }
